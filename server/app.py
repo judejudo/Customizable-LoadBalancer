112f8197ef53ca54eef793749f11e8d45e5296da
mumbiann <107537497+mumbiann@users.noreply.github.com>
2024-05-29 11:54:14 +0300

    Adding comments

--- server/app.py
+++ server/app.py
@@ -7,15 +7,19 @@ logging.basicConfig(level=logging.INFO)
 
 app = Flask(__name__)
 
+#Define route for home endpoint
 @app.route('/home', methods=['GET'])
 def home():
+    #Get server Id from environment variable or set it to unknown if not found
     server_id = os.environ.get('SERVER_ID', 'Unknown')
     return f'Hello from Server: {server_id}\n'
 
+#Define route for heartbeat endpoint
 @app.route('/heartbeat', methods=['GET'])
 def heartbeat():
     # Sends empty response with a valid response code (200 OK)
     return '', 200
 
 if __name__ == '__main__':
+    #Run the application on all available network intefaces, port 5000
     app.run(host='0.0.0.0', port=5000)
